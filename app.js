20abe983ecb6020d35fbf50b1888eb9c52188174
Rohit Mahto <rohitmahto267@gmaill.com>
2021-11-22 21:16:58 +0530

    cache policy

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.use(express.static('dist'))
+app.use(express.static('dist', { maxAge: 604800000 }));
+
+app.use(setCache)
 
 app.get('/', (req, res) => res.sendFile(path.resolve([__dirname, "dist", "index.html"])));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
