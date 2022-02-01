fa3774ffb6c1078158da0a8c3cddc7f79484666c
Rohit Mahto <rohitmahto267@gmaill.com>
2022-02-01 22:13:55 +0530

    port updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 1234;
 const path = require("path");
 
 app.use(express.static('dist', { maxAge: 604800000 }));
