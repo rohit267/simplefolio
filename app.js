5952cf4c82401495f68c17bd5355bbad2cc8d8fb
Rohit Mahto <rohit.mahto@techolution.com>
2022-09-21 23:04:05 +0530

    fix code

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const path = require("path");
 
 app.use(express.static("dist", { maxAge: 604800000 }));
 
-app.get("/", (req, res) => res.sendFile("./dist/index.html"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
 app.listen(port, () => console.log(`Portfolio app listening on port ${port}!`));
